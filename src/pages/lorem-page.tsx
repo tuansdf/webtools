9d3eeb354b93b0f549ed1952edd441f5fa7cd328
tuansdf <103062682+tuansdf@users.noreply.github.com>
2024-12-30 10:11:27 +0700

    refactor: generate lorem on submit

--- src/pages/lorem-page.tsx
+++ src/pages/lorem-page.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/layout/header.tsx";
+import { Button } from "@/components/ui/button.tsx";
 import { Input } from "@/components/ui/input.tsx";
 import { ScreenLoading } from "@/components/ui/screen-loading.tsx";
 import { Textarea } from "@/components/ui/textarea.tsx";
@@ -8,7 +9,7 @@ import { createSignal, Show } from "solid-js";
 
 export default function LoremPage() {
   const [output, setOutput] = createSignal<string>("");
-  const [wordCount, setWordCount] = createSignal<number>(3);
+  const [wordCount, setWordCount] = createSignal<number>(0);
   const [processingTime, setProcessingTime] = createSignal<number | null>(null);
   const [isLoading, setIsLoading] = createSignal(false);
 
@@ -16,20 +17,31 @@ export default function LoremPage() {
     setIsLoading(true);
     await new Promise((r) => setTimeout(r, 100));
     const start = performance.now();
-    setOutput(await generateLorem(wordCount()));
+    let count = wordCount();
+    setOutput(await generateLorem(count));
     setProcessingTime(performance.now() - start);
     setIsLoading(false);
   };
 
-  const handleSubmitDebounced = debounce(handleSubmit);
+  const handleWordCountChange = (count: number) => {
+    if (!count || count < 1) count = 1;
+    if (count > 1_000_000) count = 1_000_000;
+    setWordCount(() => count);
+  };
 
-  handleSubmit();
+  const handleSubmitDebounced = debounce(handleSubmit);
 
   return (
     <>
       <Header title="Lorem Ipsum Generator" />
 
-      <div class="container-xxl p-3">
+      <form
+        class="container-xxl p-3"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmitDebounced();
+        }}
+      >
         <div class="row row-cols-sm-2 row-cols-lg-3">
           <Input
             label="Word count"
@@ -37,20 +49,20 @@ export default function LoremPage() {
             min={1}
             max={1_000_000}
             value={wordCount()}
-            onInput={(e) => {
-              setWordCount(Number(e.currentTarget.value));
-              handleSubmitDebounced();
-            }}
+            onInput={(e) => setWordCount(Number(e.currentTarget.value))}
+            onChange={(e) => handleWordCountChange(Number(e.currentTarget.value))}
             containerClass="mb-3 col"
           />
         </div>
 
         <Textarea label="Output" value={output()} readOnly rows={12} letterCount={output().length} />
 
+        <Button type="submit">Generate</Button>
+
         <Show when={processingTime() === 0 || processingTime()}>
           <div class="text-end font-monospace mt-3">done in {processingTime()}ms</div>
         </Show>
-      </div>
+      </form>
 
       <Show when={isLoading()}>
         <ScreenLoading />
