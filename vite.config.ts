8aab90e5d9f7d095b89a5da54759bf644fb94dcc
tuansdf <103062682+tuansdf@users.noreply.github.com>
2025-03-14 01:31:02 +0700

    feat: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import solid from "vite-plugin-solid";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [solid(), tsconfigPaths()],
+  plugins: [
+    solid(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: { theme_color: "#ffffff" },
+      workbox: { maximumFileSizeToCacheInBytes: 3000000 },
+    }),
+  ],
   worker: {
     plugins: () => [tsconfigPaths()],
   },
