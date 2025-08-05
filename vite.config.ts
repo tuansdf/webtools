cfda56600b69e736434aca4cf4caa359a41411b6
tuansdf <103062682+tuansdf@users.noreply.github.com>
2025-08-05 20:39:58 +0700

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
 import viteLegacyPlugin from "@vitejs/plugin-legacy";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 import solid from "vite-plugin-solid";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    solid(),
-    tsconfigPaths(),
-    viteLegacyPlugin(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: { name: "WebTools", short_name: "WebTools", theme_color: "#ffffff" },
-      workbox: { maximumFileSizeToCacheInBytes: 3000000 },
-    }),
-  ],
+  plugins: [solid(), tsconfigPaths(), viteLegacyPlugin()],
   worker: {
     plugins: () => [tsconfigPaths()],
   },
